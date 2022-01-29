b293c73483797802d39cd4319963defc6f053fd4
KRD13 <rauchdavis13@gmail.com>
2022-01-28 19:34:39 -0500

    code snapshot 13.1"

--- models/User.js
+++ models/User.js
@@ -1,5 +1,3 @@
-const { Model } = require("sequelize/dist")
-
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
@@ -9,38 +7,33 @@ class User extends Model {}
 // define table columns and configuration
 User.init(
   {
-    // DEFINE AN ID COLUMN
+    // define an id column
     id: {
       // use the special Sequelize DataTypes object provide what type of data it is
       type: DataTypes.INTEGER,
-      // this is the equivalent of SQL's 'NOT NULL' option
+      // this is the equivalent of SQL's `NOT NULL` option
       allowNull: false,
       // instruct that this is the Primary Key
-      primaryKey: true, 
+      primaryKey: true,
       // turn on auto increment
       autoIncrement: true
     },
-    
     // define a username column
     username: {
       type: DataTypes.STRING,
       allowNull: false
     },
-
-     // define an email column
-     email: {
-       type: DataTypes.STRING,
-       allowNull: false,
-     //there cannot be any duplicate email values in this table
-     unique: true,
-     // if allNull is set to false, we can run our data through validation
-     validate: {
-       isEmail: true
-     },
-
-     
+    // define an email column
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      // there cannot be any duplicate email values in this table
+      unique: true,
+      // if allowNull is set to false, we can run our data through validators before creating the table data
+      validate: {
+        isEmail: true
+      }
     },
-    
     // define a password column
     password: {
       type: DataTypes.STRING,
@@ -50,11 +43,10 @@ User.init(
         len: [4]
       }
     }
-    
   },
 
   {
-    // TABLE CONFIGUURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
+    // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
 
     // pass in our imported sequelize connection (the direct connection to our database)
     sequelize,

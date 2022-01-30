cfcc1f4fcd260a2658eca154c3ed3019a00abed7
KRD13 <rauchdavis13@gmail.com>
2022-01-29 21:49:07 -0500

    added in comment-routes.js, updated models/Comment.js and related iindex.js and user-routes

---
+++ routes/api/comment-routes.js
@@ -0,0 +1,51 @@
+const router = require('express').Router();
+const { Comment } = require('../../models');
+
+// get all comments
+router.get('/', (req, res) => {
+  console.log('======================');
+  Comment.findAll()
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.post('/', (req, res) => {
+  // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
+  Comment.create({
+    comment_text: req.body.comment_text,
+    user_id: req.body.user_id,
+    post_id: req.body.post_id
+  })
+    .then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+});
+router.delete('/:id', (req, res) => {
+
+});
+
+router.delete('/:id', (req, res) => {
+  Post.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
